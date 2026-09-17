069f884f40e13173ac85478fc43373836e65725d
gervasiomeana-arg <gervasiomeana@gmail.com>
2026-09-17 15:49:33 -0300

    security: enforce admin middleware before application routes

--- secure-server.ts
+++ secure-server.ts
@@ -1,11 +1,13 @@
 import express from 'express';
 import { authMeRouter } from './src/server/authMeRouter';
+import { securityMiddleware } from './src/server/securityMiddleware';
 
 const expressApplication = express.application as any;
 const originalInit = expressApplication.init;
 
 expressApplication.init = function patchedInit(this: any) {
   originalInit.call(this);
+  this.use(securityMiddleware);
   this.use('/api/auth', authMeRouter);
 };
 
