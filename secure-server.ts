6404fdb62e078a58c934f16e9b870646542b1523
gervasiomeana-arg <gervasiomeana@gmail.com>
2026-09-17 16:50:19 -0300

    security: enforce server-side Mercado Pago confirmation

--- secure-server.ts
+++ secure-server.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import { authMeRouter } from './src/server/authMeRouter';
 import { securityMiddleware } from './src/server/securityMiddleware';
+import { securePaymentConfirmation } from './src/server/securePaymentConfirmation';
 
 const expressApplication = express.application as any;
 const originalInit = expressApplication.init;
@@ -8,6 +9,7 @@ const originalInit = expressApplication.init;
 expressApplication.init = function patchedInit(this: any) {
   originalInit.call(this);
   this.use(securityMiddleware);
+  this.use(securePaymentConfirmation);
   this.use('/api/auth', authMeRouter);
 };
 
