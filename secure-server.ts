766c73c2bdd9a5ed06a897e75f7004580ee8fd56
gervasiomeana-arg <gervasiomeana@gmail.com>
2026-09-17 19:58:40 -0300

    security: mount hardened Mercado Pago flow before legacy routes

--- secure-server.ts
+++ secure-server.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import { authMeRouter } from './src/server/authMeRouter';
 import { authCookieBridge } from './src/server/authCookieBridge';
 import { securityMiddleware } from './src/server/securityMiddleware';
+import { secureMercadoPago } from './src/server/secureMercadoPago';
 import { securePaymentConfirmation } from './src/server/securePaymentConfirmation';
 
 const expressApplication = express.application as any;
@@ -11,6 +12,7 @@ expressApplication.init = function patchedInit(this: any) {
   originalInit.call(this);
   this.use(authCookieBridge);
   this.use(securityMiddleware);
+  this.use(secureMercadoPago);
   this.use(securePaymentConfirmation);
   this.use('/api/auth', authMeRouter);
 };
